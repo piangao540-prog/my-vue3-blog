b21278ad5236722d5b429e9fa8d2844b2956422a
wc <iii@qq.com>
2026-05-09 19:40:02 +0800

    创建基础home页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
 import { useBlogStore } from '@/stores/blog'
-import { useRouter } from 'vue-router'
-import { ElCard, ElButton, ElTag, ElDivider } from 'element-plus'
+import { ElCard, ElDivider } from 'element-plus'
+import { useRouter } from 'vue-router';
 
 const blogStore = useBlogStore()
+const router = useRouter()
 
 </script>
 
 <template>
- 
+    <section>
+        <h1>一个博客</h1>
+        <p>用于练习vue3知识和记录技术</p>
+
+        <el-divider/>
+        <h2>随便写写</h2>
+        <el-card v-for="article in blogStore.latestArticles"  :key="article.id"
+        class="article-id" shadow="hover" @click="router.push(`/articles/${article.id}`)">
+            <h3>{{ article.title }}</h3>
+            <p>{{ article.summary }}</p>
+            <small>{{ article.createdAt }}</small>
+        </el-card>
+    </section>
 </template>
 
 <style scoped>
-
+.article-card {
+  margin-bottom: 16px;
+  cursor: pointer;
+}
 </style>
