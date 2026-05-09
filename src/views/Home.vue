7abddd58d31fc584d6bcf54cf6369ac31fcaac68
wc <iii@qq.com>
2026-05-09 18:32:22 +0800

    整体目录结构搭建

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useBlogStore } from '@/stores/blog'
+import { useRouter } from 'vue-router'
+import { ElCard, ElButton, ElTag, ElDivider } from 'element-plus'
+
+const blogStore = useBlogStore()
+
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+
+</style>
