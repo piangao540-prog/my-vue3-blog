239fb05078606b9c34991200f2cee0e182749499
wc <iii@qq.com>
2026-06-28 18:10:26 +0800

    新增请求拦截器，添加 Authorization 头以支持 token 认证

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { error } from 'console'
+import { config } from 'process'
 
 // 创建 Axios 实例
 const instance = axios.create({
@@ -8,6 +10,18 @@ const instance = axios.create({
     'Content-Type': 'application/json',
   },
 })
+
+// 请求拦截器
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
 
 // 响应拦截器
 instance.interceptors.response.use(
