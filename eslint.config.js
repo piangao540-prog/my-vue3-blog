c36977937e4965dc0d3f20a6b7b95845bb01d3dd
wc <iii@qq.com>
2026-09-14 11:36:30 +0800

    chore(lint): 接入 ESLint 依赖与扁平配置，新增 lint 脚本

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default defineConfigWithVueTs(
+    {
+        name: 'app/files-to-lint',
+        files: ['**/*.{ts,mts,tsx,vue}'],
+    },
+    {
+        name: 'app/files-to-ignore',
+        ignores: ['**/dist/**', '**/coverage/**', 'server/**'],
+    },
+    js.configs.recommended,
+    pluginVue.configs['flat/essential'],
+    vueTsConfigs.recommended,
+    skipFormatting,
+)
